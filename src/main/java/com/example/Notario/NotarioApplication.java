ff6388672c107ae2171ed18c5a8ef3be8ff267a2
Rusby6 <chyzhruslan@gmail.com>
2025-06-03 19:18:12 +0200

    Primeros cambios

---
+++ src/main/java/com/example/Notario/NotarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.Notario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Notario/NotarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
