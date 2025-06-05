a03d4589c8e386fe8dd2b0b1da43c4866cf36fea
Rusby6 <chyzhruslan@gmail.com>
2025-06-05 16:41:38 +0200

    Borro

--- src/main/java/com/example/Notario/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Notario.repository;
-
-import com.example.Notario.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
